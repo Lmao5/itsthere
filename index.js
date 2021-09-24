afb4e8532e8f2a20a8aad70bce4284c8282603a2
Lmao5 <cilipadi270@gmail.com>
2021-09-24 22:23:31 +0800

    Added itsthere function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function test(){
-    console.log('hello')
+module.exports = function(x) {
+    // Return false if input variable is null or undefined or length of string/array/object is 0 or containing just whitespaces.
+    if (x === null || x === undefined || x.length === 0 || Object.keys(x).length === 0 || !/\S/.test(x)){
+        return false
+    }
+    // Return true for otherwise
+    else{
+        return true
+    }
 }
-
-test()
